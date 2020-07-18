a89739ff4953c9ffd1ade013e31e2b6c520ae56e
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-18 10:56:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by KPS on 7/18/2020.
+ */
+public class Main {
+    public static void main(String[] Strings) {
+
+
+        double minutesInYear = 60 * 24 * 365;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the number of minutes: ");
+
+        double min = input.nextDouble();
+
+        long years = (long) (min / minutesInYear);
+        int days = (int) (min / 60 / 24) % 365;
+
+        System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
+    }
+}
+
